1432d189db479ffcc6c737fe3ae461d5f3a782e6
AlvaroHernandezM <alvaro.hernandez01@uptc.edu.co>
2017-06-12 22:22:33 -0500

    Operacion cliente

---
+++ src/logic/OperationsClient.java
@@ -0,0 +1,186 @@
+package logic;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class OperationsClient {
+
+	private ObjectInputStream inputStream; // flujo de lectura de datos
+	private ObjectOutputStream outputStream; // flujo de escritura de datos
+	private Thread reading; // hilo de lectura
+	private Thread writring; // hilo de escritura
+	private Socket conection; // socket del cual obtendra el flujo de entrada y salida
+	private MessageClient messageClient; // encargado para enviar el nombre del cliente
+	private boolean read; // controla cuando se inicia a leer y cuando se detiene
+	private boolean write; // controla cuando se inicia a escribir y cuando se detiene
+	private MessageServer messageServer; // encargado para recibir la lista de jugadores
+		
+
+	/**
+	 * 
+	 * @param socket
+	 *            sirve para alamacenar el socket
+	 */
+	public OperationsClient(Socket socket) {
+		conection = socket;
+		read = true;
+		write = true;
+	}
+
+	/**
+	 * lee constantemente
+	 */
+	public synchronized void read() {
+		reading = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				try {
+					
+					inputStream = new ObjectInputStream(
+							conection.getInputStream());
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+					//e.printStackTrace();
+				}
+
+				while (read) {
+					try {
+						if (inputStream != null) {
+							messageServer = (MessageServer) inputStream.readObject();
+							if (messageServer != null) {
+								System.out.println(messageServer.getType());
+								System.out.println(messageServer.getMessage());
+							}
+						}
+					} catch (ClassNotFoundException | IOException e) {
+						
+						// Tolerancia a Fallos va aqui
+						System.out.println("Toleranacia a Fallos");
+						closeDataFlowRead();
+						closeConection();
+						read = false;
+						//e.printStackTrace();
+					}
+
+					// ////////////////////////////////////////////////
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					// ///////////////////////////////////////////////
+				}
+			}
+		});
+
+		reading.start();
+	}
+	
+	
+
+	/**
+	 * escribe una unica vez
+	 * 
+	 * @param message
+	 *            objeto que se va a enviar
+	 */
+	public synchronized void write(MessageClient message) {
+		try {
+			if (outputStream == null) {
+				outputStream = new ObjectOutputStream(
+						conection.getOutputStream());
+			}
+			System.out.println("Se escribe mensaje tipo:"+message.getType());
+			System.out.println("Mesaje: "+message.getMessage());
+			outputStream.writeObject(message);
+
+		} catch (IOException e) {
+			System.out.println("sale");
+			e.printStackTrace();
+		}
+	}
+
+	
+	/**
+	 * escribe un mensaje constantemente
+	 * 
+	 * @param message objeto que se va a enviar
+	 */
+	public void alwaysWrite(MessageClient message) {
+		writring = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					outputStream = new ObjectOutputStream(
+							conection.getOutputStream());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				while (write) {
+					try {
+						outputStream.writeObject(message);
+					} catch (IOException e) {
+						closeDataFlowWrite();
+						closeConection();
+						write = false;
+						e.printStackTrace();
+					}
+
+					// ////////////////////////////////////////////////
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					// ///////////////////////////////////////////////
+				}
+			}
+		});
+
+		writring.start();
+	}
+	
+	
+	/**
+	 * Cierra el flujo de escritura
+	 */
+	private void closeDataFlowWrite() {
+		try {
+			write = false;
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Cierra el flujo de lectura
+	 */
+	private void closeDataFlowRead() {
+		try {
+			read = false;
+			inputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Cierra la conexion con el socket
+	 */
+	private void closeConection() {
+		try {
+			conection.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
