309e887115a738401ccfef089c7889a5569d9f6c
AlvaroHernandezM <alvaro.hernandez01@uptc.edu.co>
2017-06-14 00:10:57 -0500

    primera parte d esonido

---
+++ src/gui/Sounds.java
@@ -0,0 +1,36 @@
+
+package gui;
+import javax.sound.sampled.*;
+import java.io.*;
+import sun.audio.*;
+
+public class Sounds {
+
+	private String ruta;
+	private Clip clip;
+
+	/**
+	 * 
+	 */
+	public Sounds() {
+		super();
+		this.ruta = "../sounds/";
+		this.clip = null;
+	}
+	
+	public void sonido(String archivo){
+		try{
+			this.clip = AudioSystem.getClip();
+			this.clip.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream(this.ruta+archivo+".wav")));
+			this.clip.start();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Sounds sounds = new Sounds();
+		sounds.sonido("tada");
+		
+	}
+}
