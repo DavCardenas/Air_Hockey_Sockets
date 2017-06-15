93f613a696a88b9331cdc11444c9392b90f2560b
DavCardenas <c123davidleo@gmail.com>
2017-06-15 09:38:40 -0500

    imagenes

---
+++ src/gui/PaneGame.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+public class PaneGame extends JPanel{
+
+	private Image table;
+	private Image disc;
+	private Image playerBlue;
+	private Image playerRed;
+	
+	public static final int PLAYER_HEIGH = 50;
+}
