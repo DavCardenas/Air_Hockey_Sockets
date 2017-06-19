10ce55c16bcd218978d55d6cf15cc7a0dc9e688a
AlvaroHernandezM <alvaro.hernandez01@uptc.edu.co>
2017-06-19 15:57:20 -0500

    Adicionando variables y metodos en Player para manejar los totales

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -13,12 +13,19 @@ public class Player implements Serializable{
 	private String name; // nombre del jugador
 	private Point position; // posicion del jugador en el tablero de juego
 	private int points; // puntos ganados en el partido
+	private int totalPoints; //puntos acumulados en todos los partidos
 	private int goals; // numero de goles realizados
+	private int totalGoals; //numero de goles acumulados en todos los partidos
 	private boolean inGame; // sirve para verificar si el jugador esta jugando
 	
 	public Player() {
 		name = "";
 		position = null;
+		points = 0;
+		totalPoints = 0;
+		goals = 0;
+		totalGoals = 0;
+		inGame = false;
 	}
 	
 	/**
@@ -33,13 +40,19 @@ public class Player implements Serializable{
 		player_Aux.setPoints(p_player.getPoints());
 		player_Aux.setGoals(p_player.getGoals());
 		player_Aux.setInGame(p_player.isInGame());
-		
+		player_Aux.setTotalGoals(p_player.getTotalGoals());
+		player_Aux.setTotalPoints(p_player.getTotalPoints());
 		return player_Aux;
 	}
 	
 	public Player(String p_name, Point p_point) {
 		name = p_name;
 		position = p_point;
+		points = 0;
+		totalPoints = 0;
+		goals = 0;
+		totalGoals = 0;
+		inGame = false;
 	}
 	
 	public int getGoals() {
@@ -55,6 +68,19 @@ public class Player implements Serializable{
 	 */
 	public void addGoals() {
 		goals++;
+	}
+	
+	/**
+	 * metodo para avisar que acabo un juego y debe actualizar valores
+	 */
+	public void gameIsOver(){
+		this.inGame = false;
+		
+		this.totalGoals += this.goals;
+		this.goals= 0;
+		
+		this.totalPoints += this.points;
+		this.points = 0;		
 	}
 	
 	/**
@@ -98,6 +124,36 @@ public class Player implements Serializable{
 		this.inGame = inGame;
 	}
 	
+	
+	
+	/**
+	 * @return the totalPoints
+	 */
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+
+	/**
+	 * @param totalPoints the totalPoints to set
+	 */
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
+
+	/**
+	 * @return the totalGoals
+	 */
+	public int getTotalGoals() {
+		return totalGoals;
+	}
+
+	/**
+	 * @param totalGoals the totalGoals to set
+	 */
+	public void setTotalGoals(int totalGoals) {
+		this.totalGoals = totalGoals;
+	}
+
 	@Override
 	public String toString() {
 		return name;
